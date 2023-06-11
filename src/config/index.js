f46f17636d2d2938238dc21fdfa8f661282c87b8
michaelspinedat <stevenpnd@gmail.com>
2023-06-10 22:35:44 -0500

    Create contact section

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+  emailTemplateId: import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+  emailServiceId: import.meta.env.VITE_EMAIL_SERVICE_ID,
+  emailPublicKey: import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+}
+
+export { config }
